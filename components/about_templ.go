d47b8ab4c7e7369ffdd10e9825f6d8e409064dbf
M4tthewDE <matthias.kronberg@gmail.com>
2026-03-14 14:11:47 +0100

    feat: add source link to about page

--- components/about_templ.go
+++ components/about_templ.go
@@ -42,7 +42,7 @@ func About() templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 2, "</div>This product uses the TMDB API but is not endorsed or certified by TMDB.</div></html>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 2, "</div>This product uses the TMDB API but is not endorsed or certified by TMDB.<hr><a target=\"_blank\" href=\"https://github.com/m4tthewDE/blunt\">Source</a></div></html>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
